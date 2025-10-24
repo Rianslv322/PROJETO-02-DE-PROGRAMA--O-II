22709cbbd017e8427c6761c98be01f58430b3eea
Rianslv322 <riansilvasouza915@gmail.com>
2025-10-24 09:20:16 -0300

    Atualização do Sistema de Gerenciaento

--- PROJETO 02 DE PROGRAMAÇÃO II/Gerente.py
+++ PROJETO 02 DE PROGRAMAÇÃO II/Gerente.py
@@ -1,72 +1,106 @@
-
 import json
 from Pessoa_Funcionario import *
 
 class Gerente(Funcionario):
-    def __init__(self, nome, cpf, idade, cargo, salario_base, descontos,senha):
+    def __init__(self, nome, cpf, idade, cargo, salario_base, descontos, senha):
         super().__init__(nome, cpf, idade, cargo, salario_base, descontos)
         self.senha = senha
         self.lista_funcionario = []
+        self.funcionario = Funcionario(nome, cpf, idade, cargo, salario_base, descontos)
+        self.lista_funcionario.append(self.funcionario)
+        self.salvar()
 
-    
     def mostrar_dados(self):
-        print(f"""
-        Nome: {self.nome}
-        CPF: {self.cpf}
-        Idade: {self.idade}
-        Cargo: {self.cargo}
-        Salário: {self.salario}
-        Desconto: {self.descontos}
-        Senha: {self.senha}""")
+        return {
+            "Nome": self.nome,
+            "CPF": self.cpf,
+            "Idade": self.idade,
+            "Cargo": self.cargo,
+            "Salário": self.salario,
+            "Desconto": self.descontos,
+            "Senha": self.senha
+        }
 
-    def cadastrar_funcionaio(self, nome, cpf, idade, cargo, salario_base, descontos):
-        novo_funcionario = Funcionario(nome,cpf,idade,cargo,salario_base,descontos)
-        self.lista_funcionario.append(novo_funcionario)
-        return "Novo funcionário Cadastrado"
-
-
-    def editar_informaces(self):
-        print("""
-        [1] Editar Nome    
-        [2] Editar Cargo
-        [3] Editar Salário
-""")
-        
-        opcao = input("Opçao aqui --> ")
-
-
-        if opcao == "1":
-            novo_nome = input("Novo Nome: ")
-            self.nome = novo_nome
-            return f"Nome alterado."
-
-        if opcao == "2":
-            novo_cargo  = input("Novo Cargo: ")
-            self.cargo = novo_cargo
-            return f"Cargo alterado."
-        
-        if opcao == "3":
-            novo_salario = float(input("Novo salário: "))
-            self.salario = novo_salario
-            return "Salário alterado."
-
-    
-
-    def salvar_json(self,arquivo):
+    def salvar(self):
         lista = []
         for x in self.lista_funcionario:
-            lista.append(x.mostrar_dados())
+            lista.append(x.mostrar_dados()) 
+        with open("arquivo.json", "w", encoding="UTF-8") as dados:
+            json.dump(lista, dados, indent=4, ensure_ascii=False)
 
-        with open(arquivo,"w",encoding="UTF-8") as dados:
-            json.dump(lista,dados,indent=4,ensure_ascii=False)
+        print("Dados salvos.")
 
-        print(f"Dados salvos com sucesso em {arquivo}")
+        
 
-    
-gerente1 = Gerente("Rian","123213-213",29,"Gerente",6000,600,123)
-gerente1.cadastrar_funcionaio("Rian","13213-321321-321321",22,"Gestor",6000,200)
-gerente1.editar_informaces()
-gerente1.mostrar_dados()
+gerente1 = Gerente("Rianlindao","123213-213",29,"Gerente",6000,600,123)
+
+gerente1.salvar() 
+
+
+    # def salvar_json(self,arquivo):
+    #     lista = []
+    #     for x in self.lista_funcionario:
+    #         lista.append(x.mostrar_dados())
+
+    #     with open(arquivo,"w",encoding="UTF-8") as dados:
+    #         json.dump(lista,dados,indent=4,ensure_ascii=False)
+
+    #     print(f"Dados salvos com sucesso em {arquivo}")
+
+
+
+        # def cadastrar_funcionaio(self, nome, cpf, idade, cargo, salario_base, descontos):
+        # novo_funcionario = Funcionario(nome,cpf,idade,cargo,salario_base,descontos)
+        # self.lista_funcionario.append(novo_funcionario)
+
+        # lista = []
+        # for x in self.lista_funcionario:
+        #     lista.append(x.mostrar_dados())
+
+        # with open("arquivo.json","w",encoding="UTF-8") as dados:
+        #     json.dump(lista,dados,indent=4,ensure_ascii=False)
+
+        # print(f"Dados salvos.")
+
+
+        #parti pronta daqui pra baixo
+
+
+#     def editar_informaces(self):
+#         print("""
+#         [1] Editar Nome    
+#         [2] Editar Cargo
+#         [3] Editar Salário
+#         [4] Sair
+# """)
+#         while True:
+#             opcao = input("Opçao aqui --> ")
+
+
+#             if opcao == "1":
+#                 novo_nome = input("Novo Nome: ")
+#                 self.nome = novo_nome
+#                 return f"Nome alterado."
+
+#             if opcao == "2":
+#                 novo_cargo  = input("Novo Cargo: ")
+#                 self.cargo = novo_cargo
+#                 return f"Cargo alterado."
+            
+#             if opcao == "3":
+#                 novo_salario = float(input("Novo salário: "))
+#                 self.salario = novo_salario
+#                 return "Salário alterado."
+            
+#             if opcao == "4":
+#                 return "Todas as alterações foram concluidas."
+#                 break
+
+
+
+
+
+
 
 
 
